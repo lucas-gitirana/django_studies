905a471b078823b0e9d95106a9cb9089127a8ccb
Lucas Gitirana <gitiranalucas5@gmail.com>
2022-12-03 15:44:36 -0300

    dinamic redirection

--- challenges_website/challenges/views.py
+++ challenges_website/challenges/views.py
@@ -1,22 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 monthly_challenges = {
-    "january":"Eat no meat for the entire month!",
+    "january": "Eat no meat for the entire month!",
     "february": "Walk at least 20 minutes every day!",
     "march": "Learn Django for at least 20 minutes every day!",
-    "april":"Walk at least 20 minutes every day!",
-    "may":"Walk at least 20 minutes every day!",
-    "june":"Learn Django for at least 20 minutes every day!",
-    "july":"Eat no meat for the entire month!",
-    "august":"Eat no meat for the entire month!",
-    "september":"Eat no meat for the entire month!",
-    "october":"Learn Django for at least 20 minutes every day!",
-    "november":"Walk at least 20 minutes every day!",
+    "april": "Walk at least 20 minutes every day!",
+    "may": "Walk at least 20 minutes every day!",
+    "june": "Learn Django for at least 20 minutes every day!",
+    "july": "Eat no meat for the entire month!",
+    "august": "Eat no meat for the entire month!",
+    "september": "Eat no meat for the entire month!",
+    "october": "Learn Django for at least 20 minutes every day!",
+    "november": "Walk at least 20 minutes every day!",
     "december": "Learn Django for at least 20 minutes every day!"
 }
 
 # Create your views here.
+
 
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
@@ -25,13 +27,13 @@ def monthly_challenge_by_number(request, month):
         return HttpResponseNotFound("Invalid month!")
     else:
         month_forwarded = months[month - 1]
-        return HttpResponseRedirect("/challenges/" + month_forwarded)        
+        month_path = reverse("monthly_challenge_path", args=[month_forwarded])
+        return HttpResponseRedirect(month_path)
+
 
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
         return HttpResponse(challenge_text)
     except:
-        return HttpResponseNotFound("This month is not supported!") 
-
-    
+        return HttpResponseNotFound("This month is not supported!")
