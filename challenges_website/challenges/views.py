19dc1fe6369d61ee9501ebffc2bc9ec89df77991
Lucas Gitirana <gitiranalucas5@gmail.com>
2023-01-20 08:40:46 -0300

    404 page

--- challenges_website/challenges/views.py
+++ challenges_website/challenges/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 
 
@@ -63,4 +63,4 @@ def monthly_challenge(request, month):
             "month": month
         })     
     except:
-        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
+        raise Http404()
